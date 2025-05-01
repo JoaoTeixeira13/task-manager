3766c56612a69d62d7ef7c96c41665541b3e2ce3
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2025-05-01 18:50:27 +0200

    clean up controllers

--- src/main/java/com/taskManager/taskManager/adapter/web/controller/AuthController.java
+++ src/main/java/com/taskManager/taskManager/adapter/web/controller/AuthController.java
@@ -2,17 +2,19 @@ package com.taskManager.taskManager.adapter.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.taskManager.taskManager.adapter.web.dto.AuthTokenTO;
 import com.taskManager.taskManager.application.service.TokenService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final TokenService tokenService;
 
-    @PostMapping("/api/auth/token")
+    @PostMapping("/token")
     public AuthTokenTO generateToken() {
 
         return AuthTokenTO.builder()
--- src/main/java/com/taskManager/taskManager/adapter/web/controller/TaskController.java
+++ src/main/java/com/taskManager/taskManager/adapter/web/controller/TaskController.java
@@ -14,42 +14,43 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/tasks")
 public class TaskController {
 
     private final TaskAdapterMapper taskAdapterMapper;
     private final TaskService taskService;
 
-    @GetMapping("/api/tasks")
+    @GetMapping
     public List<TaskResponseTO> getTasks(@RequestParam(required = false) String author,
                                          @RequestParam(required = false) String assignee) {
         List<Task> tasks = taskService.getTasks(author, assignee);
         return taskAdapterMapper.mapTasks(tasks);
     }
 
-    @PostMapping("/api/tasks")
+    @PostMapping
     public TaskResponseTO addTask(@RequestBody @Valid TaskRequestTO taskRequestTO) {
         Task task = taskService.addTask(taskRequestTO.getTitle(), taskRequestTO.getDescription());
         return taskAdapterMapper.map(task);
     }
 
-    @PutMapping("/api/tasks/{taskId}/assign")
+    @PutMapping("/{taskId}/assign")
     public TaskResponseTO updateTaskAssignee(@PathVariable Long taskId, @RequestBody @Valid TaskAssignmentTO taskAssignmentTO) {
         Task task = taskService.updateTaskAssignee(taskId, taskAssignmentTO.getAssignee());
         return taskAdapterMapper.map(task);
     }
 
-    @PutMapping("/api/tasks/{taskId}/status")
+    @PutMapping("/{taskId}/status")
     public TaskResponseTO updateTaskStatus(@PathVariable Long taskId, @RequestBody @Valid TaskStatusTO taskStatusTO) {
         Task task = taskService.updateTaskStatus(taskId, TaskStatus.valueOf(taskStatusTO.getStatus().name()));
         return taskAdapterMapper.map(task);
     }
 
-    @PostMapping("/api/tasks/{taskId}/comments")
+    @PostMapping("/{taskId}/comments")
     public void addComment(@PathVariable Long taskId, @RequestBody @Valid CommentRequestTO commentTO) {
         taskService.addComment(taskId, commentTO.getText());
     }
 
-    @GetMapping("/api/tasks/{taskId}/comments")
+    @GetMapping("/{taskId}/comments")
     public List<CommentResponseTO> getTaskComments(@PathVariable Long taskId) {
         List<Comment> taskComments = taskService.getTaskComments(taskId);
         return taskAdapterMapper.mapComments(taskComments);
